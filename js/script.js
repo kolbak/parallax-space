514689345e21a7cffbe49abb76432d1fcdcda327
kolbak <53657170+kolbak@users.noreply.github.com>
2020-07-21 00:01:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const parallax = document.querySelector('.parallax-space');
+let parallaxInstanceSpace = new Parallax(parallax);
+
+const content = document.querySelector('.parallax-content');
+let parallaxInstanceTextCart = new Parallax(content);
