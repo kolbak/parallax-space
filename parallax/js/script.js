6c4a931c52b4b36070505c78c10629c3be277d1f
Kolbak <kushaevmarat@mail.ru>
2020-07-21 00:08:55 +0300

    123123

--- parallax/js/script.js
+++
@@ -1,5 +0,0 @@
-const parallax = document.querySelector('.parallax-space');
-let parallaxInstanceSpace = new Parallax(parallax);
-
-const content = document.querySelector('.parallax-content');
-let parallaxInstanceTextCart = new Parallax(content);
